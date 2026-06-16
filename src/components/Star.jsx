cf041ff3d832aef043997c08d9840389115976a5
Zi Qi <117970574+zq5q37@users.noreply.github.com>
2026-06-16 11:12:57 +0800

    max rank 5

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -5,24 +5,24 @@ import {
     PolarAngleAxis, PolarRadiusAxis
 } from 'recharts';
 
-const Star = ({stats}) => {
+const Star = ({ stats }) => {
 
-  const data = [
-    { name: 'Knowledge', x: stats.Knowledge },
-    { name: 'Guts', x: stats.Guts },
-    { name: 'Proficiency', x: stats.Proficiency },
-    { name: 'Kindness', x: stats.Kindness },
-    { name: 'Charm', x: stats.Charm },
-  ];
+    const data = [
+        { name: 'Knowledge', x: stats.Knowledge },
+        { name: 'Guts', x: stats.Guts },
+        { name: 'Proficiency', x: stats.Proficiency },
+        { name: 'Kindness', x: stats.Kindness },
+        { name: 'Charm', x: stats.Charm },
+    ];
 
-  const starData = [];
+    const starData = [];
 
     for (let i = 0; i < data.length; i++) {
         starData.push(data[i]);
-        const inBetweenValue = (data[i].x + data[(i+1)%data.length].x);
+        const inBetweenValue = (data[i].x + data[(i + 1) % data.length].x);
         const fractionalValue = inBetweenValue * 0.18;
         const iName = ' '.repeat(i);
-        starData.push({name: iName, x: fractionalValue})
+        starData.push({ name: iName, x: fractionalValue })
     };
 
     return (
@@ -30,7 +30,11 @@ const Star = ({stats}) => {
             <RadarChart height={500} width={500}
                 outerRadius="80%" data={starData}>
                 {/* <PolarGrid /> */}
-                <PolarAngleAxis dataKey="name" />
+                <PolarAngleAxis dataKey="name" tick={({ x, y, payload }) => (
+                    <text x={x} y={y} textAnchor="middle" dominantBaseline="top" fontSize={20} fill="#ffee00">
+                        {payload.value} {data.find(d => d.name === payload.value)?.x >= 5 ? '[MAX]' : ''}
+                    </text>
+                )} />
                 <PolarRadiusAxis domain={[0, 5]} tick={false} axisLine={false} />
                 <Radar dataKey="x" stroke="orange"
                     fill="yellow" fillOpacity={0.9} />
