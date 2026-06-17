c8c78696e90e8d2979652261ec76cc2dc7c28004
Zi Qi <117970574+zq5q37@users.noreply.github.com>
2026-06-17 16:55:26 +0800

    try fix header 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,10 +35,9 @@ function Header({ onReset }) {
                     <div className="logo">
                         <img src={p5Logo}></img>
                     </div>
-  
                 </div>
                 <div className='right'>
-                  <button onClick={() => { onReset(); playClick(); }}>Reset</button>
+                    <button onClick={() => { onReset(); playClick(); }}>Reset</button>
                     <button className='sound-button' onClick={() => { toggleMute(); }}>{muted ? '🔇' : '🔊'}</button>
                     {/* <img className='stats-logo' src={statsLogo}></img> */}
                 </div>
