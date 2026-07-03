a5d8e7da83a0eddf62c575087951cb52ce299226
Zi Qi <117970574+zq5q37@users.noreply.github.com>
2026-07-03 14:35:42 +0800

    try fix login for phone

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
-// src/firebase.js
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeAuth, indexedDBLocalPersistence, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,8 +14,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: indexedDBLocalPersistence,
+});
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
