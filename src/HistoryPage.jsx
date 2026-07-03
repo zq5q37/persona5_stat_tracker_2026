9e257e0a95d0af7606c3e1b28db554fd6ec8a42a
Zi Qi <117970574+zq5q37@users.noreply.github.com>
2026-07-03 15:29:08 +0800

    clear history page

--- src/HistoryPage.jsx
+++ src/HistoryPage.jsx
@@ -11,30 +11,10 @@ export default function HistoryPage({ activities }) {
       style={{ backgroundImage: `url(${redBgPic})` }}
     >
       <div className="history-container">
-        <h1>Activity History</h1>
+        <div className='edit-section-label'>
+          <span className='edit-label-text'>LOG HISTORY</span>
+        </div>
         
-        {activities && activities.length > 0 ? (
-          <div className="activities-list">
-            {activities.map((activity, index) => (
-              <div key={index} className="activity-item">
-                <h3>{activity.name}</h3>
-                {activity.traits && activity.traits.length > 0 ? (
-                  <div className="traits-list">
-                    {activity.traits.map((trait, i) => (
-                      <span key={i} className="trait-badge">
-                        {trait}
-                      </span>
-                    ))}
-                  </div>
-                ) : (
-                  <p>No traits</p>
-                )}
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p className="no-activities">No activities logged yet.</p>
-        )}
 
         <button
           className="dialogue-button back-button"
